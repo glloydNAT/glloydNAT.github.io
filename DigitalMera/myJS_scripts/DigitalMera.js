8ddc11a84c2a06fae8c358f232dd35e691aa12c4
glloydNAT <lloyd.business.holdings@gmail.com>
2025-04-20 18:32:29 -0600

    update Digital Mera

--- DigitalMera/myJS_scripts/DigitalMera.js
+++ DigitalMera/myJS_scripts/DigitalMera.js
@@ -1,58 +1,144 @@
-async function getGeojson(zipfile) {
+/* 
+Digital Mera: Population Change in the Rio Grande Glaze-paint Area
+Garrett Lloyd, Gavin Schorr, Benno Lee
+*/
 
-    //for the shapefiles in the files folder called pandr.shp
-    const gj = await shp("data/regionShapes/" + zipfile);
-    // console.log(geojson['features'][0]);
-    L.geoJSON(gj['features'][0]).addTo(mymap);
+const regionShapesArray = [], sitesShapesArray = [];
+
+const regions =[
+    ["1_Piro.zip","#fff5eb"],
+    ["1a_Piro.zip","#fff5eb"],
+    ["2_Tompiro.zip","#fee6ce"],
+    ["3_Jumano.zip","#fdd0a2"],
+    ["3a_Jumano.zip","#fdd0a2"],
+    ["4_WestTiwa.zip","#fdae6b"],
+    ["4a_WestTiwa.zip","#fdae6b"],
+    ["5_EastTiwa.zip","#fd8d3c"],
+    ["6_Keres.zip","#f16913"],
+    ["7_TanoTowa.zip","#d94801"],
+    ["7a_TanoTowa.zip","#d94801"],
+    ["8_NorthTiwa.zip","#8c2d04"]
+]
+
+//define the map
+var mymap = L.map('mapId',{
+    
+    fullscreenControl: {
+        pseudoFullscreen: true // if true, fullscreen to page width and height
+    }
+}).setView([35.08539970090307, -106.62437969380134], 7.5);
 
 
-    return 0;
+async function getShapeGeojson(zipfile, colorCode) {
+    //get the shapefiles
+    try {
+        const gj = await shp("data/regionShapes/" + zipfile);
+        
+        // define the regional shape (outline) style
+        var thisStyle={
+            "color": colorCode,
+            "weight":2,
+            "opacity": 0.5
+        }
+      
+        // add the shape
+        L.geoJSON(gj['features'][0], {
+            style: thisStyle
+        }).addTo(mymap);
 
+        return 0;
+    } catch (error) {
+        console.log(error.message);
+        return 1;
+    }
 }
 
-const regionShapesArray = [];
-regionShapesArray[0] = getGeojson("1_Piro.zip");
-regionShapesArray[1] = getGeojson("1a_Piro.zip");
-regionShapesArray[2] = getGeojson("2_Tompiro.zip");
-regionShapesArray[3] = getGeojson("3_Jumano.zip");
-regionShapesArray[4] = getGeojson("3a_Jumano.zip");
-regionShapesArray[5] = getGeojson("4_WestTiwa.zip");
-regionShapesArray[6] = getGeojson("4a_WestTiwa.zip");
-regionShapesArray[7] = getGeojson("5_EastTiwa.zip");
-regionShapesArray[8] = getGeojson("6_Keres.zip");
-regionShapesArray[9] = getGeojson("7_TanoTowa.zip");
-regionShapesArray[10] = getGeojson("7a_TanoTowa.zip");
-regionShapesArray[11] = getGeojson("8_NorthTiwa.zip");
+async function getSiteGeojson(zipfile, period) {
+    var marker;
+    try {
+        //get the shapefile
+        const gj = await shp("data/siteShapes/" + zipfile);
 
+        // process per-period features in shapefile 
+        gj["features"].forEach(feature => {
+            if (feature['properties']['Periods'].includes(period)) {
+                var lat = parseFloat(feature['geometry']['coordinates'][1]);
+                var long = parseFloat(feature['geometry']['coordinates'][0]);
+                marker = L.marker([lat, long]);
+                marker.addTo(mymap);
+                marker.bindPopup("Site Number: " + feature['properties']['SiteNo']);
+            }
+        });
+        return 0;
+    } catch (error) {
+        console.log(error.message);
+        return 1;
+    }
+}
+
+// function called by the timeline slider changing
+changeMapFunction = async function ({ label, value, map }) {    
+
+    // removes previous layers 
+    map.eachLayer(function (layer) {
+        if (layer instanceof L.Marker) {
+            map.removeLayer(layer);
+        }
+    });
+
+    // add the sites per period    
+    switch (value){
+        case "1":
+            getSiteGeojson("04_West-Tiwa-all.zip","1")
+            break;
+        case "2":
+            getSiteGeojson("04_West-Tiwa-all.zip","2")
+            break;
+        case "3":
+            getSiteGeojson("04_West-Tiwa-all.zip","3")
+            break;    
+        case "4":
+            getSiteGeojson("04_West-Tiwa-all.zip","4")
+            break;
+        case "5":
+            getSiteGeojson("04_West-Tiwa-all.zip","5")
+            break;
+        } 
+}
+
+// MAIN ROUTINE BEGINS HERE
+
+// add the region shapes
+for (let i=0;i < regions.length;i++){
+    regionShapesArray[i] = getShapeGeojson(regions[i][0],regions[i][1]);
+}
+
+// make sure regionShapeArrays are all present.
 if (regionShapesArray.includes(1)) {
     console.log("error in regionShapes array");
 } else {
-    console.log("all good in regionShapes array");
+    console.log("all good in arrays");
 }
 
-
-var mymap = L.map('mapId').setView([35.08539970090307, -106.62437969380134], 17);
-
-
-// create a variable named "osm" that is used to hold OpenStreetMap tile layer
-// var osm = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-
-/*
-var osm = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
-minZoom: 4,
-maxZoom: 7,
-attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-ext: 'jpg'
-});*/
+// define our timeline slider
+// https://github.com/svitkin/leaflet-timeline-slider/
+L.control.timelineSlider({
+    timelineItems: ["Period 1", "Period 2", "Period 3", "Period 4", "Period 5"],
+    initializeChange: true,
+    changeMap: changeMapFunction
+}).addTo(mymap);
 
 
 
-var osm = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
-    maxZoom: 7,
+// TODO: basemap changer
+
+// define our default basemap
+var osm = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', { 
     attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
 });
 
 
-
 // add the OpenStreetMap tile layer (i.e. the variable osm above) to the map
 osm.addTo(mymap);
+
+
