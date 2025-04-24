1c7b41392433dd0e4482fe6f4ad3204ced06e5c9
glloydNAT <lloyd.business.holdings@gmail.com>
2025-04-24 10:32:29 -0600

    update for beta

--- DigitalMera/myJS_scripts/DigitalMera.js
+++ DigitalMera/myJS_scripts/DigitalMera.js
@@ -5,30 +5,53 @@ Garrett Lloyd, Gavin Schorr, Benno Lee
 
 const regionShapesArray = [], sitesShapesArray = [];
 
+// define our regions
 const regions =[
-    ["1_Piro.zip","#fff5eb"],
-    ["1a_Piro.zip","#fff5eb"],
-    ["2_Tompiro.zip","#fee6ce"],
-    ["3_Jumano.zip","#fdd0a2"],
-    ["3a_Jumano.zip","#fdd0a2"],
-    ["4_WestTiwa.zip","#fdae6b"],
-    ["4a_WestTiwa.zip","#fdae6b"],
-    ["5_EastTiwa.zip","#fd8d3c"],
-    ["6_Keres.zip","#f16913"],
-    ["7_TanoTowa.zip","#d94801"],
-    ["7a_TanoTowa.zip","#d94801"],
-    ["8_NorthTiwa.zip","#8c2d04"]
+    ["1_Piro.zip","#fff5eb"], //0
+    ["1a_Piro.zip","#fff5eb"], //1
+    ["2_Tompiro.zip","#fee6ce"], //2
+    ["3_Jumano.zip","#fdd0a2"], //3
+    ["3a_Jumano.zip","#fdd0a2"], //4
+    ["4_WestTiwa.zip","#fdae6b"], //5
+    ["4a_WestTiwa.zip","#fdae6b"], //6
+    ["5_EastTiwa.zip","#fd8d3c"], //7
+    ["6_Keres.zip","#f16913"], //8
+    ["7_TanoTowa.zip","#d94801"], //9
+    ["7a_TanoTowa.zip","#d94801"], //10
+    ["8_NorthTiwa.zip","#8c2d04"] //1
+]
+
+// define our sites
+const sites = [
+    [],
+    [],
+    [],
+    ["04_West-Tiwa-all.zip"],
+    [],
+    [],
+    [],
+    [],
+    ["diagnosticSites-all.zip"]
 ]
 
 //define the map
 var mymap = L.map('mapId',{
     
     fullscreenControl: {
-        pseudoFullscreen: true // if true, fullscreen to page width and height
-    }
+        pseudoFullscreen: true, // if true, fullscreen to page width and height
+        position: "topright"
+    },
+    zoomControl: false
+
+
 }).setView([35.08539970090307, -106.62437969380134], 7.5);
 
+// custom zoom controls
+L.control.zoom({
+    position: "topright"
+}).addTo(mymap);
 
+// get and convert the *individual* region shapefiles into geojson
 async function getShapeGeojson(zipfile, colorCode) {
     //get the shapefiles
     try {
@@ -53,9 +76,26 @@ async function getShapeGeojson(zipfile, colorCode) {
     }
 }
 
+// get and convert the *individual* site shapefiles into geojson
 async function getSiteGeojson(zipfile, period) {
     var marker;
     try {
+        // custom icon
+        var iconOptions = {
+            iconUrl: "../myCss_styleFiles/DM_icon.png",
+            iconSize: [25,25]
+        }
+        var customIcon = L.icon(iconOptions);
+
+        // marker options
+        var markerOptions = {
+            clickable: true,
+            draggable: false,
+            icon: customIcon
+        }
+
+
+
         //get the shapefile
         const gj = await shp("data/siteShapes/" + zipfile);
 
@@ -64,7 +104,7 @@ async function getSiteGeojson(zipfile, period) {
             if (feature['properties']['Periods'].includes(period)) {
                 var lat = parseFloat(feature['geometry']['coordinates'][1]);
                 var long = parseFloat(feature['geometry']['coordinates'][0]);
-                marker = L.marker([lat, long]);
+                marker = L.marker([lat, long], markerOptions);
                 marker.addTo(mymap);
                 marker.bindPopup("Site Number: " + feature['properties']['SiteNo']);
             }
@@ -77,6 +117,7 @@ async function getSiteGeojson(zipfile, period) {
 }
 
 // function called by the timeline slider changing
+// this will invoke the getSiteGeoJson function
 changeMapFunction = async function ({ label, value, map }) {    
 
     // removes previous layers 
@@ -87,28 +128,31 @@ changeMapFunction = async function ({ label, value, map }) {
     });
 
     // add the sites per period    
-    switch (value){
-        case "1":
-            getSiteGeojson("04_West-Tiwa-all.zip","1")
-            break;
-        case "2":
-            getSiteGeojson("04_West-Tiwa-all.zip","2")
-            break;
-        case "3":
-            getSiteGeojson("04_West-Tiwa-all.zip","3")
-            break;    
-        case "4":
-            getSiteGeojson("04_West-Tiwa-all.zip","4")
-            break;
-        case "5":
-            getSiteGeojson("04_West-Tiwa-all.zip","5")
-            break;
-        } 
+    for (let i=0;i < sites.length;i++){
+        switch (value){
+            case "1":
+                getSiteGeojson(sites[i][0],value)
+                break;
+            case "2":
+                getSiteGeojson(sites[i][0],value)
+                break;
+            case "3":
+                getSiteGeojson(sites[i][0],value)
+                break;    
+            case "4":
+                getSiteGeojson(sites[i][0],value)
+                break;
+            case "5":
+                getSiteGeojson(sites[i][0],value)
+                break;
+            } 
+     }
 }
+
 
 // MAIN ROUTINE BEGINS HERE
 
-// add the region shapes
+// add all the region shapes
 for (let i=0;i < regions.length;i++){
     regionShapesArray[i] = getShapeGeojson(regions[i][0],regions[i][1]);
 }
@@ -120,6 +164,11 @@ if (regionShapesArray.includes(1)) {
     console.log("all good in arrays");
 }
 
+
+
+
+
+
 // define our timeline slider
 // https://github.com/svitkin/leaflet-timeline-slider/
 L.control.timelineSlider({
@@ -128,7 +177,94 @@ L.control.timelineSlider({
     changeMap: changeMapFunction
 }).addTo(mymap);
 
+// define our nav control
+new L.Control.BootstrapDropdowns({
+    position: "topleft",
+    className: "legend",
+    menuItems: [
+        {
+            html: '<i class="fas fa-map-marked-alt"></i>About',
+            title: "About",
+            afterClick: ()=>{
+                splashScreen.show();
+            }
+        }
+    ],
+}).addTo(mymap);
 
+// define our legend
+
+var polygonSides = '7';
+
+var legendItems = [
+    {
+        label: "Site",
+        type: "image",
+        url: "../myCss_styleFiles/DM_icon.png",
+    },
+    {
+        label: "Piro",
+        type: "polygon",
+        sides: polygonSides,
+        color: regions[0][1],
+        fill: true,
+    },
+    {
+        label: "Tompiro",
+        type: "polygon",
+        sides: polygonSides,
+        color: regions[2][1],
+        fill: true,
+    },
+    {
+        label: "Jumano",
+        type: "polygon",
+        sides: polygonSides,
+        color: regions[3][1],
+        fill: true,
+    },
+    {
+        label: "West Tiwa",
+        type: "polygon",
+        sides: polygonSides,
+        color: regions[5][1],
+        fill: true,
+    },
+    {
+        label: "East Tiwa",
+        type: "polygon",
+        sides: polygonSides,
+        color: regions[7][1],
+        fill: true,
+    },
+    {
+        label: "Keres",
+        type: "polygon",
+        sides: polygonSides,
+        color: regions[8][1],
+        fill: true,
+    },
+    {
+        label: "Tano Towa",
+        type: "polygon",
+        sides: polygonSides,
+        color: regions[9][1],
+        fill: true,
+    },
+    {
+        label: "North Tiwa",
+        type: "polygon",
+        sides: polygonSides,
+        color: regions[11][1],
+        fill: true,
+    }
+];
+
+L.control.Legend({
+    position: "bottomleft",
+    legends: legendItems,
+    column: 2
+}).addTo(mymap);
 
 // TODO: basemap changer
 
